4b7a56317c0d78691aad19e561de1fbe87520070
AlinaZakirod <gabie1989@gmail.com>
2019-09-12 22:23:36 -0400

    add meal plan

--- mealPrep/public/javascripts/new-plan.js
+++ mealPrep/public/javascripts/new-plan.js
@@ -1,9 +1,6 @@
  document.getElementById("newPlanForm").onsubmit = function(e){
     e.preventDefault();
 
-    // alert('hello')
-
-
     let planResultDiv = $('divForPlanResults')
     planResultDiv.html('')
 
@@ -26,41 +23,23 @@
     .then(response => {
       console.log('post successful and the response is: ', response.data );
       // debugger;
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[0].recipe.label}</h4><img src='${response.data.hits[0].recipe.image}'<br><p>Calories: ${response.data.hits[0].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[1].recipe.label}</h4><img src='${response.data.hits[1].recipe.image}'<br><p>Calories: ${response.data.hits[1].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[2].recipe.label}</h4><img src='${response.data.hits[2].recipe.image}'<br><p>Calories: ${response.data.hits[1].recipe.calories}</p><form action="/plans/create" method="POST"><button class="addMealFromApi">Add To The Plan</button></form>`)
+      $('#divForPlanResults').prepend(`<h4>${response.data.hits[0].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[0].recipe.image}'<br><p>Calories: ${response.data.hits[0].recipe.calories}</p><form><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+      $('#divForPlanResults').prepend(`<h4>${response.data.hits[1].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[1].recipe.image}'<br><p>Calories: ${response.data.hits[1].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+      $('#divForPlanResults').prepend(`<h4>${response.data.hits[2].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[2].recipe.image}'<br><p>Calories: ${response.data.hits[2].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+
+      $('body').on('click', '.addMealFromApi', function(event){
+        event.preventDefault()
+        console.log("nah");
+        const target = $(event.target)
+        target.text( "Added" );
+        const parent = target.parent().parent()
+        $(`parent :nth-child(1)`).css( "background-color", "red" );
+      })
+      
     })
     .catch(error => {
       console.log('Oh No! Error is: ', error);  
     })
-    
-        // function addMealFromApi() {
-        //   axios.post('http://localhost:3000/meals/create', {
-        //     title: title?,
-        //     kcal: kcal?
-        //   })
-        //   .then(result => {
-        //       console.log("Then I need to push it into       plan._menuObjects array and display success message ", result)
-        //   })
-        //   .catch(err => {
-        //     console.log('error while adding meal from API to db:' + err)
-        // })
 
-       const addButton = document.getElementsByClassName("addMealFromApi") 
-      
-       function put(e){
-        let print = e.currentTarget.previousSibling.innerHTML
-        console.log(print)
-      }
-      put(addButton)
   }
 
-
-
-
-
-
-
-
-
- 
