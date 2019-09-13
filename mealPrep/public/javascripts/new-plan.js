c911dccb052ed1360b6eba785a817820f7940093
AlinaZakirod <gabie1989@gmail.com>
2019-09-13 06:02:44 -0400

    spilt response from API into 3 boxes

--- mealPrep/public/javascripts/new-plan.js
+++ mealPrep/public/javascripts/new-plan.js
@@ -23,17 +23,35 @@
     .then(response => {
       console.log('post successful and the response is: ', response.data );
       // debugger;
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[0].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[0].recipe.image}'<br><p>Calories: ${response.data.hits[0].recipe.calories}</p><form><form><button class="addMealFromApi">Add To The Plan</button></form>`)
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[1].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[1].recipe.image}'<br><p>Calories: ${response.data.hits[1].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
-      $('#divForPlanResults').prepend(`<h4>${response.data.hits[2].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[2].recipe.image}'<br><p>Calories: ${response.data.hits[2].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+      $('#divForPlanResults').prepend('<div id="mealResultBoxOne" class="mealResultBox" style="width:30%; display:inline;float:left"></div>')
+      $('#mealResultBoxOne').append(`<h4>${response.data.hits[0].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[0].recipe.image}'<br><p>Calories: ${response.data.hits[0].recipe.calories}</p><form><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+
+      $('#divForPlanResults').prepend('<div id="mealResultBoxTwo" class="mealResultBox" style="width:30%; display:inline;float:left"></div>')
+      $('#mealResultBoxTwo').append(`<h4>${response.data.hits[1].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[1].recipe.image}'<br><p>Calories: ${response.data.hits[1].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
+
+      $('#divForPlanResults').prepend('<div id="mealResultBoxThree" class="mealResultBox" style="width:30%; display:inline;float:left"></div>')
+      $('#mealResultBoxThree').append(`<h4>${response.data.hits[2].recipe.label}</h4><img class ="newmealImage" src='${response.data.hits[2].recipe.image}'<br><p>Calories: ${response.data.hits[2].recipe.calories}</p><form><button class="addMealFromApi">Add To The Plan</button></form>`)
 
       $('body').on('click', '.addMealFromApi', function(event){
         event.preventDefault()
         console.log("nah");
         const target = $(event.target)
         target.text( "Added" );
-        const parent = target.parent().parent()
-        $(`parent :nth-child(1)`).css( "background-color", "red" );
+        const parent = target.parent().parent().css( "background-color", "red" );
+        target.parent().prev().css( "background-color", "blue" );
+
+        const mealTitle = target.parent().prev().prev().prev().text();
+        console.log(mealTitle)
+    
+        const mealKcal = target.parent().prev().text();
+        console.log(mealKcal)
+
+        const newMealFromApi = new Object()
+        newMealFromApi.title = mealTitle;
+        mealTitle.
+
+        
+        
       })
       
     })
